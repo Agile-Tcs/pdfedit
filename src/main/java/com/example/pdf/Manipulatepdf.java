7a6301827824e9884d215a59fda459359b23854e
Agile-Tcs <Agile-Tcs@users.noreply.github.com>
2020-07-31 13:49:48 -0400

    Initial commit

---
+++ src/main/java/com/example/pdf/Manipulatepdf.java
@@ -0,0 +1,93 @@
+package com.example.pdf;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.logging.log4j.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+
+public class Manipulatepdf {
+	private static final long serialVersionUID = 3149151943127976520L;
+	private static final Logger logger = LogManager.getLogger(Manipulatepdf.class);
+	private final static ByteArrayOutputStream os = new ByteArrayOutputStream();	
+	public static final SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
+	
+	/**
+	 * Constructor
+	 */
+	public static void main(String args[]) {
+		// Start with our document template
+		try {
+			String fileName = "src\\main\\resources\\0032a_orig.pdf";
+			String newFileName = "src\\main\\resources\\0032a_updated.pdf";
+			File myFile = new File(fileName);
+			PDDocument pdDoc = PDDocument.load(myFile);
+			if (pdDoc.isEncrypted()) {
+		        try {
+		        	//pdDoc.decrypt("");
+		        	//pdDoc.setAllSecurityToBeRemoved(true);
+		        }
+		        catch (Exception e) {
+		            throw new Exception("The document is encrypted, and we can't decrypt it.", e);
+		        }
+		    }
+	        PDDocumentCatalog pdCatalog = pdDoc.getDocumentCatalog();
+	        PDAcroForm acroForm = pdCatalog.getAcroForm();
+	
+	        if (acroForm == null) {
+	            System.out.println("No form-field --> stop");
+	            return;
+	        }
+	
+	        List<PDField> fields = acroForm.getFields();
+	        //printFields(acroForm);
+	        // set the text in the form-field <-- does work
+	        for (PDField field : fields) {
+	        	//System.out.println(field.getFullyQualifiedName());
+				if (field.getFullyQualifiedName().equals("First Name")) {
+				  field.setValue("Manas");
+				  field.setReadOnly(false);
+				}
+				if (field.getFullyQualifiedName().equals("Last Name")) {
+					  field.setValue("Sahu");
+					  field.setReadOnly(false);
+				}
+	        }		
+	        /*
+	         *AccessPermission accessPermission = new AccessPermission();
+			 * accessPermission.setCanModify(true);
+			 * 
+			 * StandardProtectionPolicy standardProtectionPolicy = new
+			 * StandardProtectionPolicy("", "", accessPermission);
+			 * pdDoc.protect(standardProtectionPolicy);
+			 */
+	        pdDoc.save(fileName);
+	        pdDoc.close();
+        } catch (Exception e2) {
+			logger.error("Error while creating the updated pdf file.", e2);
+		}
+	}
+
+	private static void printFields(PDAcroForm acroForm) {
+		List fields = null;
+		fields = acroForm.getFields();
+		Iterator fieldsItr = fields.iterator();
+
+		while(fieldsItr.hasNext()) {
+			PDField field = (PDField)fieldsItr.next();
+			System.out.println(field.getPartialName());
+		}
+	}
+}
